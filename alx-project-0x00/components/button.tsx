ec2b415064a778bbd0fd833a3edaf11b1dd570e2
favouriweala <favouriweala@gmail.com>
2025-11-30 11:12:28 +0100

    fix

--- alx-project-0x00/components/button.tsx
+++ alx-project-0x00/components/button.tsx
@@ -5,12 +5,13 @@ interface ButtonProps {
   styles: string
 }
 
-const Button: React.FC<ButtonProps> = ({ title, className }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button
-      className={`px-4 py-2 font-medium text-white bg-blue-600 ${className}`}
+      className={`px-4 py-2 font-medium text-white bg-blue-600 ${styles}`}
     >
       {title}
+      {styles}
     </button>
   )
 }
